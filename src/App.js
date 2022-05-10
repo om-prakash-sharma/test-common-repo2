c8d66c6f905fcea42a8a8d2fc10aaae38e4cfba1
Om Prakash Sharma <om@habilelabs.io>
2022-05-10 20:41:12 +0530

    - update code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 
-import Header from "./components/header";
-import Footer from "./components/footer";
-import Sidebar from "./components/sidebar";
+import Header from "./common-code/components/header";
+import Footer from "./common-code/components/footer";
+import Sidebar from "./common-code/components/sidebar";
 
 function App() {
   return (
     <div className="App">
-      <Header ></Header>
+      <Header title="App 2" ></Header>
       <main>
         <Sidebar></Sidebar>
         <content-area>
         </content-area>
       </main>
-      <Footer></Footer>
+      <Footer title="App 2" ></Footer>
     </div>
   );
 }
